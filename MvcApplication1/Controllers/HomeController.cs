e416a23c0ddc4dc3759e86777012cde477a9bfbd
lizzy <kudutest@outlook.com>
2013-07-18 11:01:25 +0800

    testname

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "My awesome ASP.NET MVC application with changes.";
+            ViewBag.Message = "Lizzy's awesome ASP.NET MVC application with changes.";
 
             return View();
         }
